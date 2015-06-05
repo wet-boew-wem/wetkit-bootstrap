e83dbbc4a57db5942ee3df65c578a348d691f8fd
William Hearn <sylus1984@gmail.com>
2015-06-04 21:31:35 -0400

    wetkit bootstrap breadcrumbs showing duplicate titles

--- templates/system/breadcrumb.vars.php
+++ templates/system/breadcrumb.vars.php
@@ -18,10 +18,21 @@ function wetkit_bootstrap_preprocess_breadcrumb(&$variables) {
 
   if (theme_get_setting('bootstrap_breadcrumb_title') && !empty($breadcrumb)) {
     $item = menu_get_item();
-    $breadcrumb[] = array(
-      // If we are on a non-default tab, use the tab's title.
-      'data' => !empty($item['tab_parent']) ? check_plain($item['title']) : drupal_get_title(),
-      'class' => array('active'),
-    );
+
+    $page_title = !empty($item['tab_parent']) ? check_plain($item['title']) : drupal_get_title();
+
+    // Handle wetkit_bootstrap not completely overriding Bootstrap itself.
+    $last_breadcrumb = end($breadcrumb);
+    if ($page_title == $last_breadcrumb['data']) {
+      array_pop($breadcrumb);
+    }
+
+    if (!empty($page_title)) {
+      $breadcrumb[] = array(
+        // If we are on a non-default tab, use the tab's title.
+        'data' => $page_title,
+        'class' => array('active'),
+      );
+    }
   }
 }
