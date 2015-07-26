f56af6517be7ec286b68eef3036f9fb4a6336d23
William Hearn <sylus1984@gmail.com>
2015-07-26 12:37:14 -0400

    Issue #2533860: disabling "Show 'Home' breadcrumb link" in wetkit_bootstrap arbitrarily pops off first breadcrumb element

--- templates/system/breadcrumb.vars.php
+++ templates/system/breadcrumb.vars.php
@@ -10,12 +10,6 @@
 function wetkit_bootstrap_preprocess_breadcrumb(&$variables) {
   $breadcrumb = &$variables['breadcrumb'];
 
-  // Optionally get rid of the homepage link.
-  $show_breadcrumb_home = theme_get_setting('bootstrap_breadcrumb_home');
-  if (!$show_breadcrumb_home) {
-    array_shift($breadcrumb);
-  }
-
   if (theme_get_setting('bootstrap_breadcrumb_title') && !empty($breadcrumb)) {
     $item = menu_get_item();
 
