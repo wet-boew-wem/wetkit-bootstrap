8b953e1af152d84a96c3dd44f23b506386599658
William Hearn <sylus1984@gmail.com>
2016-01-11 17:50:23 -0500

    Issue #2556733: Update to latest bootstrap 7.x-3.4
    Signed-off-by: William Hearn <sylus1984@gmail.com>

--- templates/system/html.vars.php
+++ templates/system/html.vars.php
@@ -53,7 +53,7 @@ function wetkit_bootstrap_preprocess_html(&$variables, $hook) {
   // BODY element attributes.
   $variables['body_attributes_array'] = array(
     'role'  => 'document',
-    'class' => &$variables['classes_array'],
+    'class' => $variables['classes_array'],
   );
   $variables['body_attributes_array'] += $variables['attributes_array'];
 
